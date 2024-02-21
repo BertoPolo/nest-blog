0a28cea942ffd2aa16f17e5028b372a47b9f08fb
Alberto Polo <bertopolo91@gmail.com>
2024-02-21 13:25:30 +0100

    setting first part of mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ModuleTestModule } from './module_test/module_test.module';
 import { ControllerTestController } from './controller_test/controller_test.controller';
 import { ServiceTestService } from './service_test/service_test.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ModuleTestModule],
+  imports: [ModuleTestModule, MongooseModule.forRoot(process.env.DB_URI)],
   controllers: [AppController, ControllerTestController],
   providers: [AppService, ServiceTestService],
 })
